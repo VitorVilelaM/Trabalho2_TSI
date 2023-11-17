b0e950948c192b68929794b4915502f162bd77da
Vitor Vilela <vilelavitor305@gmail.com>
2023-11-17 19:49:15 -0300

    Corrigindo o erro de acertar tudo

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from xgboost import XGBRegressor
+from xgboost import XGBClassifier
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.inspection import permutation_importance
+
 
 import numpy as np
 
@@ -28,14 +34,31 @@ result = featuresTest['B'].values
 caminho_arquivo = './src/ML/output.csv'
     
 ## Random Forest
-def rf_holdout():
+
+def rf_holdout_Regressor():
+    rf_train_input = train.drop(columns=['Date','B', 'CP', 'CP-1', 'CP-2', 'CP-3', 'CP-4', 'CP-5'])  # Features
+    rf_train_output = rf_train_input.pop('R') # Target
+
+    rf_test_input = test.drop(columns=['Date','B', 'CP', 'CP-1', 'CP-2', 'CP-3', 'CP-4', 'CP-5'])  # Features
+    rf_test_output = rf_test_input.pop('R') # Target
+
+    model = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=2)
+    model.fit(rf_train_input, rf_train_output)
+
+    predicts = model.predict(rf_test_input)
+
+    mae = mean_absolute_error(rf_test_output, predicts)
+
+    print(f"MAE RF Regressor: ", mae)
+
+def rf_holdout_Classifier():
     # Random Forest com Holdout
-    rf_train_input = train.drop(columns=['Date','R'])  # Features
+    rf_train_input = train.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
     rf_train_output = rf_train_input.pop('B') # Target
 
     date =  test['Date'].values
 
-    rf_test_input = test.drop(columns=['Date','R'])  # Features
+    rf_test_input = test.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
     rf_test_output = rf_test_input.pop('B') # Target
 
     # Criando o meu Baseline
@@ -45,20 +68,20 @@ def rf_holdout():
 
     RandomForest = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=2)
     RandomForest.fit(rf_train_input, rf_train_output.values)
-        
+
     predict = RandomForest.predict(rf_test_input)
 
+    
+    accuracy_baseline = accuracy_score(rf_test_output, baseline)
     accuracy_model = accuracy_score(rf_test_output, predict)
     auc_model = roc_auc_score(rf_test_output, predict)
     precision_model = precision_score(rf_test_output, predict)
     recall_model = recall_score(rf_test_output, predict)
 
-    print("Acurácia do Modelo:", accuracy_model)
-    print("AUC:", auc_model)
-    print("Recall:", recall_model)
-    print("Precision:", precision_model)
+    print("Acurácia do RF:", accuracy_model)
+    print("Baseline: ", accuracy_baseline)
 
-def rf_janela_deslizante(num_days):
+def rf_janela_deslizante_Classifier(num_days):
     # Random Forest com Janela Deslizante  
     jd_train_input = train.drop(columns=['Date','R'])  # Features
     jd_train_output = jd_train_input.pop('B') # Target
@@ -126,38 +149,47 @@ def rf_janela_deslizante(num_days):
 
 ## Rede Neural - MLP
 
-def mlp_holdout():
-    mlp_train_input = train.drop(columns=['Date','B'])  # Features
+def mlp_holdout_Regressor():
+    mlp_train_input = train.drop(columns=['Date','B', 'CP', 'CP-1', 'CP-2', 'CP-3', 'CP-4', 'CP-5'])  # Features
     mlp_train_output = mlp_train_input.pop('R') # Target
 
-    mlp_test_input = test.drop(columns=['Date','B'])  # Features
+    mlp_test_input = test.drop(columns=['Date','B', 'CP', 'CP-1', 'CP-2', 'CP-3', 'CP-4', 'CP-5'])  # Features
     mlp_test_output = mlp_test_input.pop('R') # Target
 
     baseline = [0]
     for i in range(1, mlp_test_output.shape[0]):
         baseline.append(mlp_test_output.values[i - 1])
 
-    mlp = MLPRegressor(hidden_layer_sizes=(10, 5),
-                    max_iter=5000,
-                    learning_rate_init=0.1,
-                    solver="lbfgs",
-                    activation="logistic",
-                    learning_rate="constant",
-                    )
+
+    mlp = MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=500, learning_rate_init=0.1, activation="tanh",random_state= 42)
     
     # Treine o modelo
-    mlp.fit(mlp_train_input,mlp_train_output)    
+    mlp.fit(mlp_train_input, mlp_train_output)    
     predict = mlp.predict(mlp_test_input)
 
     
     mae = mean_absolute_error(mlp_test_output,predict)
-    mse = mean_squared_error(mlp_test_output,predict)
-    rmse = np.sqrt(mse)
+    
+    print("MAE MLP Regressor: {:.2f}".format(mae))
 
-    print("Valor de MAE: {:.2f}".format(mae))
-    print("Valor de RMSE: {:.2f}".format(rmse))
+def mlp_holdout_Classifier():
+    mlp_train_input = train.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
+    mlp_train_output = mlp_train_input.pop('B') # Target
 
-def mlp_janela_deslizante(num_days):
+    mlp_test_input = test.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
+    mlp_test_output = mlp_test_input.pop('B') # Target
+
+    mlp = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=500,learning_rate_init=0.1, activation='', random_state=42)
+
+    # Treine o modelo
+    mlp.fit(mlp_train_input,mlp_train_output)    
+    predict = mlp.predict(mlp_test_input)
+
+    accuracy_model = accuracy_score(mlp_test_output, predict)
+
+    print("Acurácia do MLP:", accuracy_model)
+
+def mlp_janela_deslizante_Regressor(num_days):
     
     jd_train_input = train.drop(columns=['Date','B'])  # Features
     jd_train_output = jd_train_input.pop('R') # Target
@@ -223,58 +255,96 @@ def mlp_janela_deslizante(num_days):
     print(f"Valor de MAE: {mae}")
     print(f"Valor de RMSE: {rmse}")
 
-
 ## XGBoost 
 
-def xgboost_Regressor_holdout():
-    xgboost_train_input = train.drop(columns=['Date','B'])  # Features
+def xgboost_holdout_Regressor():
+    xgboost_train_input = train.drop(columns=['Date','B', 'CP', 'CP-1', 'CP-2', 'CP-3', 'CP-4', 'CP-5'])  # Features
     xgboost_train_output = xgboost_train_input.pop('R') # Target
 
-    xgboost_test_input = test.drop(columns=['Date','B'])  # Features
+    xgboost_test_input = test.drop(columns=['Date','B', 'CP', 'CP-1', 'CP-2', 'CP-3', 'CP-4', 'CP-5'])  # Features
     xgboost_test_output = xgboost_test_input.pop('R') # Target
 
     model = XGBRegressor()
     model.fit(xgboost_train_input, xgboost_train_output, verbose=True)
     predictions = model.predict(xgboost_test_input)
 
-    print("Erro médio absoluto: {:.2f}".format(mean_absolute_error(predictions, xgboost_test_output)))
+    print("MAE XGBoost Regressor: {:.2f}".format(mean_absolute_error(predictions, xgboost_test_output)))
+
+def xgboost_holdout_Classifier():
+    xgboost_train_input = train.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
+    xgboost_train_output = xgboost_train_input.pop('B') # Target
+
+    xgboost_test_input = test.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
+    xgboost_test_output = xgboost_test_input.pop('B') # Target
+
+    model = XGBClassifier()
+    model.fit(xgboost_train_input, xgboost_train_output)
+    predictions = model.predict(xgboost_test_input)
+
+    accuracy_model = accuracy_score(xgboost_test_output, predictions)
+    print("Acurácia do XGBoost:", accuracy_model)
 
 # KNN
 
 def knn_holdout_Regressor():
-    knn_train_input = train.drop(columns=['Date','B'])  # Features
+    knn_train_input = train.drop(columns=['Date','B', 'CP', 'CP-1', 'CP-2', 'CP-3', 'CP-4', 'CP-5'])  # Features
     knn_train_output = knn_train_input.pop('R') # Target
 
-    knn_test_input = test.drop(columns=['Date','B'])  # Features
+    knn_test_input = test.drop(columns=['Date','B', 'CP', 'CP-1', 'CP-2', 'CP-3', 'CP-4', 'CP-5'])  # Features
     knn_test_output = knn_test_input.pop('R') # Target
 
-    knn_regressor = KNeighborsRegressor(n_neighbors=5)
+    model = KNeighborsRegressor(n_neighbors=5)
 
-    knn_regressor.fit(knn_train_input, knn_train_output)
-    predicts = knn_regressor.predict(knn_test_input)
+    model.fit(knn_train_input, knn_train_output)
+    predicts = model.predict(knn_test_input)
 
     mae = mean_absolute_error(knn_test_output, predicts)
-    print(mae)
+    print("MAE KNN Regressor: ", mae)
 
+def knn_holdout_Classifier():
+    knn_train_input = train.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
+    knn_train_output = knn_train_input.pop('B') # Target
+
+    knn_test_input = test.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
+    knn_test_output = knn_test_input.pop('B') # Target
+
+    model = KNeighborsClassifier()
+    model.fit(knn_train_input, knn_train_output)
+    predictions = model.predict(knn_test_input)
+
+    accuracy_model = accuracy_score(knn_test_output, predictions)
+    print("Acurácia do KNN:", accuracy_model)
+  
 # Naive Bayes  
 
-def naive_bayes_houdolt_Regressor():
-    nb_train_input = train.drop(columns=['Date','R'])  # Features
+def naive_bayes_houdolt_Classifier():
+    nb_train_input = train.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
     nb_train_output = nb_train_input.pop('B') # Target
 
-    nb_test_input = test.drop(columns=['Date','R'])  # Features
+    nb_test_input = test.drop(columns=['Date','R', 'R-1', 'R-2', 'R-3', 'R-4', 'R-5'])  # Features
     nb_test_output = nb_test_input.pop('B') # Target
     
     model = GaussianNB()
     model.fit(nb_train_input, nb_train_output)
-
     predicts = model.predict(nb_test_input)
+    print(predicts)
     accuracy = accuracy_score(nb_test_output, predicts) 
 
-    print(accuracy)   
+    print("Acuracia Naive Bayes: ", accuracy)   
+
+def execute_Classifiers():    
+    rf_holdout_Classifier()
+    mlp_holdout_Classifier()
+    knn_holdout_Classifier()
+    xgboost_holdout_Classifier()
+    naive_bayes_houdolt_Classifier()
+
+def execute_Regressors():
+    rf_holdout_Regressor()
+    mlp_holdout_Regressor()
+    knn_holdout_Regressor()
+    xgboost_holdout_Regressor()
 
 
-naive_bayes_houdolt_Regressor()
+execute_Regressors()
 
-    
-       
